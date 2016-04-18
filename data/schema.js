2ea796addf03370701832f61152ea17c8b3d52fe
Jonas Helfer <helfer@meteor.com>
2016-04-18 14:13:58 -0700

    step 1: define schema

--- data/schema.js
+++ data/schema.js
@@ -1,10 +1,45 @@
 const typeDefinitions = `
-type Query {
-  testString: String
+type Author {
+  id: Int! # the ! means that every author object _must_ have an id
+  firstName: String
+  lastName: String
+  posts: [Post] # the list of Posts by this author
 }
 
+type Post {
+  id: Int!
+  tags: [String]
+  title: String
+  text: String
+  author: Author
+}
+
+# the schema allows the following two queries:
+type RootQuery {
+  author(firstName: String, lastName: String): Author
+  posts(tag: String): [Post]
+}
+
+# this schema allows the following two mutations:
+type RootMutation {
+  createAuthor(
+    firstName: String!
+    lastName: String!
+  ): Author
+
+  createPost(
+    tags: [String!]!
+    title: String!
+    text: String!
+    authorId: Int!
+  ): Post
+}
+
+# we need to tell the server which types represent the root query
+# and root mutation types. We call them RootQuery and RootMutation by convention.
 schema {
-  query: Query
+  query: RootQuery
+  mutation: RootMutation
 }
 `;
 
