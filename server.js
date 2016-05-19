a9661cb646c486ea4ce1c60f4f12ce838e0f9ea9
Jonas Helfer <jonas@helfer.email>
2016-05-18 18:30:51 -0700

    change how tracer is passed to apolloServer

--- server.js
+++ server.js
@@ -4,23 +4,20 @@ import Schema from './data/schema';
 import Mocks from './data/mocks';
 import Resolvers from './data/resolvers';
 import { Tracer, addTracingToResolvers, makeExecutableSchema } from 'graphql-tools';
-
+import uuid from 'node-uuid';
 
 const GRAPHQL_PORT = 8080;
 
-const tracer = new Tracer('T1');
-
-const jsSchema = makeExecutableSchema({ typeDefs: Schema, resolvers: Resolvers })
-addTracingToResolvers(jsSchema, tracer);
-
 var graphQLServer = express();
-graphQLServer.use('/', apolloServer({
-  graphiql: true,
-  pretty: true,
-  schema: jsSchema,
-  logFn: tracer.log.bind(tracer),
-  //mocks: Mocks,
-}));
+const tracer = new Tracer({ TRACER_APP_KEY: 'EBF41910-AE88-43FA-886B-FE9D4446A13A' });
+graphQLServer.use('/graphql', apolloServer({
+    graphiql: true,
+    pretty: true,
+    schema: Schema,
+    resolvers: Resolvers,
+    tracer,
+    //mocks: Mocks,
+  }));
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
 ));
