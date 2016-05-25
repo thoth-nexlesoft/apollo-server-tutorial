c5ed44c787e7398055aeb540558d301a13898e27
Jonas Helfer <helfer@users.noreply.github.com>
2016-05-25 00:14:36 -0700

    Change path from / to /graphql

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import Resolvers from './data/resolvers';
 const GRAPHQL_PORT = 8080;
 
 var graphQLServer = express();
-graphQLServer.use('/', apolloServer({
+graphQLServer.use('/graphql', apolloServer({
   graphiql: true,
   pretty: true,
   schema: Schema,
@@ -15,5 +15,5 @@ graphQLServer.use('/', apolloServer({
   //mocks: Mocks,
 }));
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
-  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
+  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
 ));
